fb7d37bd5b87fedaa6e1365a7c463adc5bc58bd1
Pierre Benjamin <pierre.benjamin@airbus.com>
2017-05-26 10:52:26 +0200

    Ajout du diamètre

--- EWIS_func.py
+++ EWIS_func.py
@@ -1,17 +1,13 @@
-import numpy as np
 import openturns as ot
-from   openturns.viewer import View
-
+from openturns.viewer import View
+import numpy as np
 #[Nb_PSU, Delta_V*V0],(np.array(Dist_SPDB))[:,0],Catalog_Gauge
-
 class CalcMassFunc(ot.OpenTURNSPythonFunction):
-
         def __init__(self, x1, x2, x3):
             super(CalcMassFunc,self).__init__(len(x2)*x1[0],1)
-            self.x1 = x1             # x1[0]: Total nb of equipment power requests, x1[1]: Constant deltaV*V0
-            self.x2 = x2             # Vector of distances b/t EPDC & element of the EWIS architecture
+            self.x1 = x1             # x1[0]: Total nb of equipment requesting power, x1[1]: Constant deltaV*V0
+            self.x2 = x2             # Vector of distances b/t EPDC & components of the EWIS architecture
             self.x3 = np.array(x3)   # Catalog of available gauges
-
         def _exec(self, X):
             # Reshaping the sample of Power request vector
             R = np.array(X).reshape(len(self.x2), self.x1[0])    
@@ -25,14 +21,18 @@ class CalcMassFunc(ot.OpenTURNSPythonFunction):
             # Get the indexes corresponding to adequate gauge            
             indexes = [(self.x3[:,0] * (self.x3[:,0] <= t)).argmax() for t in out]
             # Compute the sum of mass computed by multiplying distances with lineic mass corresponding to chosen gauge
+            Mass = np.sum(np.array(self.x2).ravel() * self.x3[indexes, 1])
+            print "Mass=", Mass
+            # Compute the diameter of the bundle corresponding to the sum of all diameters of elementary bundles
+            Diameter = np.sum(np.array(self.x3[indexes, 2]**2))
+            #Res = (Mass, Diameter)
+            #return (Mass, Diameter)
             return [np.sum(np.array(self.x2).ravel() * self.x3[indexes, 1])]
         
 class DistanceFunc(ot.OpenTURNSPythonFunction):
-
     def __init__(self,X0):
         super(DistanceFunc, self).__init__(len(X0),1)
         self.X0 = ot.NumericalPoint(X0)
-
     def _exec(self,x):
         d = ot.NumericalPoint(x) - self.X0
         return [d.norm1()]
